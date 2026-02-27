e8f8b7009e5c35961cf62b828802961d6fd4a8ac
Md. Asaduzzman Arabin <aznarabin@gmail.com>
2026-02-27 16:38:12 +0600

    fix: remove type module, add tsconfig, fix port mismatch

--- server.ts
+++ server.ts
@@ -28,6 +28,8 @@ app.use(limiter);
 
 const server = http.createServer(app);
 
-server.listen(process.env.PORT || 5000, () => {
-  console.log(`Server is running on port ${process.env.PORT || 4000}`);
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
