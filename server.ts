63a460905116e6a1bc6cc4a4c82567872ee2d5dc
Md. Asaduzzman Arabin <aznarabin@gmail.com>
2026-02-27 16:03:25 +0600

    Intial setup of the project

---
+++ server.ts
@@ -0,0 +1,33 @@
+import express from "express";
+import http from "http";
+import morgan from "morgan";
+import { rateLimit } from "express-rate-limit";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.use(morgan("dev"));
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  }),
+);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
+app.use(limiter);
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 4000}`);
+});
